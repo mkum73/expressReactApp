350690a6e89cdb23e7c497051896976fb9cad292
Manish Kumar <manish32.nith@gmail.com>
2016-02-28 01:31:35 +0530

    changing hamburger nav to close

--- temp/app.js
+++ temp/app.js
@@ -1,15 +1,15 @@
-var slideMenu = function(elem,intialState,finalState){
+var slideMenu = function(elem,intialState,finalState,this){
 
 	if(elem.hasClass('active')){
 		elem.removeClass('active');
-		$(this).removeClass('active');
+		this.removeClass('active');
 		elem.animate({
 			'opacity':0,
 			'width':intialState
 		},800);
 	}else{
 		elem.addClass('active');
-		$(this).addClass('active');
+		this.addClass('active');
 		elem.animate({
 			'opacity':1,
 			'width':finalState
@@ -18,11 +18,11 @@ var slideMenu = function(elem,intialState,finalState){
 }
 $('.navbar-header .fa-bars').on('click',function(event) {
 	event.preventDefault();
-	slideMenu($('.side-menu'),0,'85%');
+	slideMenu($('.side-menu'),0,'85%',$(this));
 });
 
 $('.navbar-header .fa-shopping-cart').on('click',function(event) {
 	event.preventDefault();
-	slideMenu($('.shopping-cart-box'),0,'85%');
+	slideMenu($('.shopping-cart-box'),0,'85%',$(this));
 });
 
