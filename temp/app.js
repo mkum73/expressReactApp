36db7609a35ce0bc5f61a162d2ea74628fed3a44
Manish Kumar <manish32.nith@gmail.com>
2016-02-28 00:31:19 +0530

    slide menu added

---
+++ temp/app.js
@@ -0,0 +1,28 @@
+var slideMenu = function(elem,intialState,finalState){
+
+	if(elem.hasClass('active')){
+		elem.removeClass('active');
+		$(this).removeClass('active');
+		elem.animate({
+			'opacity':0,
+			'width':intialState
+		},800);
+	}else{
+		elem.addClass('active');
+		$(this).addClass('active');
+		elem.animate({
+			'opacity':1,
+			'width':finalState
+		},800);
+	}
+}
+$('.navbar-header .fa-bars').on('click',function(event) {
+	event.preventDefault();
+	slideMenu($('.side-menu'),0,'85%');
+});
+
+$('.navbar-header .fa-shopping-cart').on('click',function(event) {
+	event.preventDefault();
+	slideMenu($('.shopping-cart-box'),0,'85%');
+});
+
