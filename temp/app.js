361ff37231ea4a7ebb849b036d0e6c33e0996fa2
Manish Kumar <manish32.nith@gmail.com>
2016-03-01 22:08:57 +0530

    search box UI correction

--- temp/app.js
+++ temp/app.js
@@ -1,15 +1,15 @@
-var slideMenu = function(elem,intialState,finalState){
+var slideMenu = function(elem,intialState,finalState,context){
 
 	if(elem.hasClass('active')){
 		elem.removeClass('active');
-		//this.removeClass('active');
+		context.removeClass('active');
 		elem.animate({
 			'opacity':0,
 			'width':intialState
 		},800);
 	}else{
 		elem.addClass('active');
-		//this.addClass('active');
+		context.addClass('active');
 		elem.animate({
 			'opacity':1,
 			'width':finalState
@@ -21,42 +21,25 @@ var slideMenu = function(elem,intialState,finalState){
 
 $('.navbar-header .fa-bars').on('click',function(event) {
 	event.preventDefault();
-	slideMenu($('.side-menu'),0,'85%');
+	slideMenu($('.side-menu'),0,'85%',$(this));
 });
 
 $('.navbar-header .fa-shopping-cart').on('click',function(event) {
 	event.preventDefault();
-	slideMenu($('.shopping-cart-box'),0,'85%');
+	slideMenu($('.shopping-cart-box'),0,'85%',$(this));
 });
 
 $('.navbar-header .fa-search').on('click',function(event) {
 	event.preventDefault();
-	$('.search-box').slideDown(500);
+	var elem = $(this);
+	if(elem.hasClass('active')){
+		elem.removeClass('active');
+		$('.search-box').slideUp(500);
+	}else{
+		elem.addClass('active');
+		$('.search-box').slideDown(500);
+	}
 });
 
 
-$('.add-cart').on('click',function(event){
-	event.preventDefault();
-	addToCart(productId);
-});
-
-	var cartItems = [];
-var addToCart = function(){
-	cartItems.push(getProductDetails(productId));
-};
-
-
-
-var getProductDetails = function(productId){
-	var product_details = {
-      "img_url": "placeholder.png",
-      "product_name": "T shirt",
-      "offer_price": 742,
-      "price": 4200,
-      "offer": "10%",
-      "rating": 4
-    }
-    product_details = JSON.stringify(product_details);
-    return product_details;
-};
 
