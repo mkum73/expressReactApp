ed3850ce8b51e93c80631fffbff1d64a94b1bb65
mkum73 <manish32.nith@gmail.com>
2016-05-05 21:11:05 +0530

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,15 @@ var babelify = require('babelify');
 var browserify = require('browserify');
 var reactify = require('reactify');*/
 
-gulp.task('live-server',function(){
+gulp.task('default',function(){
 	var server = new LiveServer('server/main.js');
 	server.start();
 });
-
+/*
 gulp.task('serve', ['live-server'], function() {
 	browserSync.init(null, {
 		proxy: "http://localhost:3000",
 		port: 9001
 	});
 });
+*/
